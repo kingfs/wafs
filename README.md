7a8d363dbdd0a90e373a45b05b749c380b9c9bad
kingfs <zhenzhen086@126.com>
2023-06-02 16:32:08 +0800

    feat: 放弃Naxsi构建

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 ## 支持的开源WAF
 
 1. [ModSecurity](https://github.com/SpiderLabs/ModSecurity)
-2. [Naxsi](https://github.com/nbs-system/naxsi)
+2. [TADK](https://hub.docker.com/r/intel/tadk-waf)
+3. ~~[Naxsi](https://github.com/nbs-system/naxsi)~~ 太垃圾了，不搞了
 
 *欢迎提交其他开源waf项目*
--- TADK/app/tadk/start_nginx.sh
+++ TADK/app/tadk/start_nginx.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-# export TADK_ROOT="/root/tadk_v23.03.0"
-# export LD_LIBRARY_PATH=$TADK_ROOT/build-root/install-root/lib:$LD_LIBRARY_PATH
-# export LD_LIBRARY_PATH=$TADK_ROOT/extern/daal/lib:$LD_LIBRARY_PATH
-# export LD_LIBRARY_PATH=$TADK_ROOT/extern/openvino/lib:$LD_LIBRARY_PATH
-# export LD_LIBRARY_PATH=$TADK_ROOT/extern/hs/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$TADK_LD_LIBRARY_PATH:$LD_LIBRARY_PATH
 
 if [ "${WORK_MODEL}" == "dl" ]; then
+    export TADK_MODEL_DIR=$TADK_ROOT/model/anomaly_detect/dl_model
     echo "export TADK_MODEL_DIR=$TADK_ROOT/model/anomaly_detect/dl_model" >> /etc/profile
 else
+    export TADK_MODEL_DIR=$TADK_ROOT/model/anomaly_detect/ml_model
     echo "export TADK_MODEL_DIR=$TADK_ROOT/model/anomaly_detect/ml_model" >> /etc/profile
 fi
 
